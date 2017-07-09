f2377868f3ea651db44d5b909351b3325de2bc6e
Eder Sosa <eder.sohe@gmail.com>
2017-07-08 22:13:17 -0500

    put into module for use AMI

--- src/ami.cr
+++ src/ami.cr
@@ -2,115 +2,107 @@ require "socket"
 require "secure_random"
 require "logger"
 
-module Aliases
+module AMI
   alias Event = Hash(String, String)
   alias Handler = Proc(Event, Nil)
-end
 
-
-class AMI
-  include Aliases
-
-  @client = TCPSocket.allocate
-  @@logger = Logger.new(STDOUT)
-  @@logger.level = Logger::Severity.from_value((ENV["LOG_LEVEL"]? || "1").to_i)
-
-  def initialize(host : String, port : Int32, logger : Logger = @@logger)
-    @@logger = logger
-    @@logger.level = Logger::Severity.from_value((ENV["LOG_LEVEL"]? || "1").to_i)
-
-    begin
-      @client = TCPSocket.new(host, port)
-    rescue ex : Errno
-      log.fatal(ex, "AMI::initialize")
-      exit(1)
-    end
-
-    @handlers = {} of String => AMI::Handler
-
-    sleep(1)
-    read_event("\r\n")
-
-    spawn do
-      loop do
-        read_event
-      end
-    end
-  end
-
-  def close : Nil
-    @client.close
-    exit(0)
-  end
-
-  def log : Logger
-    @@logger
-  end
+  @@log = Logger.new(STDOUT)
+  @@log.level = Logger::Severity.from_value((ENV["LOG_LEVEL"]? || "1").to_i)
 
   def self.log : Logger
-    @@logger
+    @@log
   end
 
-  def self.gen_id
-    SecureRandom.hex
+  def self.gen_id : String
+    "#{Time.utc_now.epoch_ms}-#{SecureRandom.hex(10)}"
   end
 
-  def self.info_handler(event : AMI::Event) : Nil
-      AMI.log.info("\r\n#{event}\r\n", "AMI::info_handler")
+  def self.info_handler(event : Event) : Nil
+    log.info("\r\n#{event}\r\n", "info_handler")
   end
 
-  def self.dummy_handler(event : AMI::Event) : Nil
+  def self.dummy_handler(event : Event) : Nil
   end
 
-  def parse_event(stream : String) : AMI::Event
-    event = AMI::Event.new
-    begin
-      stream.split("\r\n").each do |line|
-        token = line.split(": ", 2)
-        event[token[0]] = token[1]
+  class Client
+
+    @client = TCPSocket.allocate
+
+    def initialize(host : String, port : Int32)
+      begin
+        @client = TCPSocket.new(host, port)
+      rescue ex : Errno
+        AMI.log.fatal(ex, "intialize")
+        exit(1)
       end
-    rescue
-      event = {"RawEvent" => stream}
-    end
-    event
-  end
 
-  def read_event(delimiter : String = "\r\n\r\n") : Nil
-    stream = @client.gets(delimiter, chomp=true)
-    if stream
-      log.debug("\r\n#{stream}\r\n", "AMI::read_event")
-      @handlers.each_key do |key|
-        if stream && /#{key}/ === stream
-          @handlers[key].call(parse_event(stream))
-          if key.index("Response: (.*\r\n)*ActionID: (.*\r\n)*")
-            @handlers.delete(key)
+      @handlers = {} of String => AMI::Handler
+
+      sleep(1)
+      read_event("\r\n")
+
+      spawn do
+        loop do
+          read_event
+        end
+      end
+    end
+
+    def close : Nil
+      @client.close
+      exit(0)
+    end
+
+    def parse_event(stream : String) : AMI::Event
+      event = AMI::Event.new
+      begin
+        stream.split("\r\n").each do |line|
+          token = line.split(": ", 2)
+          event[token[0]] = token[1]
+        end
+      rescue
+        event = {"RawEvent" => stream}
+      end
+      event
+    end
+
+    def read_event(delimiter : String = "\r\n\r\n") : Nil
+      stream = @client.gets(delimiter, chomp=true)
+      if stream
+        AMI.log.debug("\r\n#{stream}\r\n", "read_event")
+        @handlers.each_key do |key|
+          if stream && /#{key}/ === stream
+            @handlers[key].call(parse_event(stream))
+            if key.index("Response: (.*\r\n)*ActionID: (.*\r\n)*")
+              @handlers.delete(key)
+            end
           end
         end
       end
     end
-  end
 
-  def add_pattern_handler(pattern : String, handler : AMI::Handler) : Nil
-      @handlers[pattern] = handler
-  end
+    def add_pattern_handler(pattern : String, handler : AMI::Handler) : Nil
+        @handlers[pattern] = handler
+    end
 
-  def send_action(name : String,
-                  actionid : String = AMI.gen_id,
-                  variables : Hash(String, String) = {} of String => String,
-                  handler : AMI::Handler | Nil = nil,
-                  **params) : String
-    message = "ActionID: #{actionid}\r\nAction: #{name.capitalize}\r\n"
-    params.each do |key, value|
-      message += "#{key.to_s.capitalize}: #{value}\r\n"
+    def send_action(name : String,
+                    actionid : String = AMI.gen_id,
+                    variables : Hash(String, String) = {} of String => String,
+                    handler : AMI::Handler | Nil = nil,
+                    **params) : String
+      message = "ActionID: #{actionid}\r\nAction: #{name.capitalize}\r\n"
+      params.each do |key, value|
+        message += "#{key.to_s.capitalize}: #{value}\r\n"
+      end
+      variables.each do |key, value|
+        message += "Variable: #{key}=#{value}\r\n"
+      end
+      if handler
+        add_pattern_handler("Response: (.*\r\n)*ActionID: #{actionid}(.*\r\n)*", handler)
+      end
+      AMI.log.debug("\r\n#{message}", "send_action")
+      @client << "#{message}\r\n"
+      actionid
     end
-    variables.each do |key, value|
-      message += "Variable: #{key}=#{value}\r\n"
-    end
-    if handler
-      add_pattern_handler("Response: (.*\r\n)*ActionID: #{actionid}(.*\r\n)*", handler)
-    end
-    log.debug("\r\n#{message}", "AMI::send_action")
-    @client << "#{message}\r\n"
-    actionid
   end
 end
